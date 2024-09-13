4edfa6840aab55e015cc0fc1f8e6311038a0e8c4
Imran Farooq <imran_farooq7@hotmail.com>
2024-09-13 21:53:58 +0500

    user create action updated with prisma

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,37 @@
 "use server";
+
+import prisma from "@/prisma/db";
+import bcrypt from "bcrypt";
+
 export const registerUser = async (data: {
 	email: string;
 	password: string;
 	confirmPassword: string;
-}) => {};
+}) => {
+	const hashedPassword = await bcrypt.hash(data.password, 12);
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: data.email,
+				password: hashedPassword,
+			},
+		});
+		if (user) {
+			return {
+				statusbar: "success",
+				message: "User created successfully",
+			};
+		}
+	} catch (error: any) {
+		if (error.code === "23505") {
+			return {
+				statusbar: "error",
+				message: "User email already exists",
+			};
+		}
+		return {
+			statusbar: "error",
+			message: "User creation failed",
+		};
+	}
+};
